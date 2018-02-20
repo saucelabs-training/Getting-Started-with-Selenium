d35081ffc4c109708e9c8d5ac6da7e38489b424e
Titus Fortner <titusfortner@gmail.com>
2018-02-20 14:03:44 -0600

    wait and act

--- src/test/SeleniumExamples.java
+++ src/test/SeleniumExamples.java
@@ -3,18 +3,26 @@ package test;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import static junit.framework.TestCase.assertEquals;
 
 public class SeleniumExamples extends Base {
 
     @Test
-    public void findElement() {
+    public void takeAction() {
         driver.get("http://address-book-example.herokuapp.com");
 
-        WebElement id = driver.findElement(By.id("sign-in"));
-        WebElement css = driver.findElement(By.cssSelector("#sign-in"));
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement signIn =  wait.until(
+                ExpectedConditions.presenceOfElementLocated(
+                        By.id("sign-in")));
 
-        assertEquals(id, css);
+        signIn.click();
+
+        assertEquals("Address Book - Sign In", driver.getTitle());
     }
 
 }
+
