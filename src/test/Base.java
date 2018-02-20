2918b9d84127b4a1454214735ce11889ea0a21ef
Titus Fortner <titusfortner@gmail.com>
2018-02-20 14:04:16 -0600

    use TestWatcher to record a result

--- src/test/Base.java
+++ src/test/Base.java
@@ -3,6 +3,9 @@ package test;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,6 +13,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Base {
 
     WebDriver driver;
+
+    @Rule
+    public TestWatcher watcher = new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, Description description) {
+            System.out.println(description.getMethodName() + ": Failed");
+        }
+
+        @Override
+        protected void succeeded(Description description) {
+            System.out.println(description.getMethodName() + ": Succeeded");
+        }
+    };
 
     @Before
     public void setup() {
