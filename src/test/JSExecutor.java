f70f6b701cb4b122204cc63e89f5d52a1f855bd3
Titus Fortner <titusfortner@gmail.com>
2018-02-21 16:27:01 -0600

    Execute JavaScript

---
+++ src/test/JSExecutor.java
@@ -0,0 +1,24 @@
+package test;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+public class JSExecutor extends Base {
+
+    @Test
+    public void jsExecute() {
+        driver.get("http://address-book-example.herokuapp.com");
+
+        WebElement element = driver.findElement(By.id("sign-in"));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", element);
+
+        js.executeScript("window.scrollBy(0,50)");
+
+    }
+
+}
+
